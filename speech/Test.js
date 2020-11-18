b6847b27684aa6eaa37f002fff5d276a44cbae62
Chris <hearmewhisper@gmail.com>
2020-11-17 22:07:15 -0500

    speech finish

--- speech/Test.js
+++ speech/Test.js
@@ -4,16 +4,25 @@ if ('speechSynthesis' in window) {
 }else{
     alert('error')
 }
+let num = 1;
 // alert("test");
 var msg = new SpeechSynthesisUtterance(); 
 var voices = window.speechSynthesis.getVoices(); 
-msg.voice = voices[10]; 
+// msg.voice = voices[01]; 
 msg.volume = 1; 
 // From 0 to 1 
 msg.rate = 1; 
 // From 0.1 to 10 
 msg.pitch = 0; 
 // From 0 to 2 
-msg.text = "1"; 
+msg.text = num; 
 msg.lang = 'en';
-speechSynthesis.speak(msg);
+// 
+function test(){
+document.getElementById("out").onclick = "";
+document.getElementById("out").innerHTML = num;
+msg.text = num;
+    speechSynthesis.speak(msg);
+    num= num+1;
+    document.getElementById("out").onclick = "out()";
+}
