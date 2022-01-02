a55aff3250e86174970921030239a4becc69e16c
HearMeWhisper <HearMeWhisper@gmail.com>
2022-01-02 11:45:05 -0500

    new

---
+++ animal/script.js
@@ -0,0 +1,11 @@
+let fish = [];
+fish = [{
+    color: "light blue",
+    value: "1",
+    img: ""
+},
+{
+    color: "light blue",
+    value: "2",
+    img: ""
+}]
--- script.js
+++ script.js
@@ -1,9 +1,12 @@
     var app = document.getElementById('type');
-
+ let scroll = () => {
+document.getElementById("type").scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+  }
 var typewriter = new Typewriter(document.getElementById('type'), {
   loop: false,
   cursor: "_",
   delay: 150,
+  // onCreateTextNode: scroll,
 });
 
 typewriter
@@ -21,18 +24,24 @@ typewriter
   .pasteString("<span id='user'>user@localhost:</span>")
   .typeString(' why does this exist?')
   .pauseFor(2000)  
-  .pasteString('<br>')
+  .pasteString('<br onload="scroll()">')
+  .callFunction(scroll)
   .pasteString('this is a collection of games and utilities that i wrote on my learning path')
   .pasteString('<br>')
-  .pasteString("<span id='user'>user@localhost:</span>")
+  .pasteString("<span onload='scroll()' id='user'>user@localhost:</span>")
+  .callFunction(scroll)
   .typeString(' ls')
   .pauseFor(2000)
   .pasteString('<br>')
+  .callFunction(scroll)
   .pasteString('<a class="link" href="/resume/">resume</a>  <a class="link" href="/yatzee/">yatzee</a>  <a class="link" href="/tictac/">tic tac touch</a><br>')
   .pasteString('<a class="link" href="/MTG/">mtg life counter</a>  <a class="link" href="/youtube_convert/">youtube downloader</a>  <a class="link" href="/binary/">binary to ascii converter</a>')
    .pasteString('<br>')
+   .callFunction(scroll)
   .pasteString("<span id='user'>user@localhost:</span>")
   .start();
 
+
+ 
 
 //    .pasteString("user@localhost:")  Computer Science Student and Enthusist
