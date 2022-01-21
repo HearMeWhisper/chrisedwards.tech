3ce6cdce41625cc54536065a8f121bbd5c8571bc
chris <hearmewhisper@gmail.com>
2022-01-21 10:46:22 -0500

    animal

--- animal/script.js
+++ animal/script.js
@@ -8,4 +8,24 @@ fish = [{
     color: "light blue",
     value: "2",
     img: ""
-}]
+    }];
+
+// Get the element
+let elem = document.querySelector('.card');
+
+// Update the ID and add a class
+// clone.classList.add('text-large1');
+
+// Inject it into the DOM
+for (let i = 0; i < 100; i++){
+    // elem.after(clone);
+    let clone = elem.cloneNode(true);
+    elem.after(clone);
+    // elem.parentNode.appendChild(clone)
+}
+
+$('.card').on('click', function (e) {
+    e.preventDefault();
+
+    $(this).toggleClass('flipped');
+});
