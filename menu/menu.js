e9cde19e66eb67ba1a7caf1ca5f08652639db00c
chris <hearmewhisper@gmail.com>
2022-03-02 09:15:51 -0500

    fix nato

--- menu/menu.js
+++ menu/menu.js
@@ -30,6 +30,7 @@ var sites = [
     ["/MTG/", "MTG"],
     ["/youtube_convert/", "Youtube Converter"],
     ["/binary/", "Binary Converter"],
+    ["/nato/", "NATO Phonetic Alphabet"],
     ["/rollDice", "Dice"],
     ["/be_mine/", "Be Mine!"],
     ["/media/Movies", "Movies"],
