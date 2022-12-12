5f57db6c158ffb40af5a3db0cd1cbae372bc0244
chris <chris@chrisedwards.tech>
2022-12-12 18:36:07 -0500

    updates

--- rollDice/dice.js
+++ rollDice/dice.js
@@ -8,25 +8,44 @@ function newDiceNumber() {
         die_value[i] = new Array(2);
     }
     for (i = 0, j = 1; i < numberOfDice; i++, j++) {
-        die_value[i][0] = "<img src='../dice/die" + j + ".png' />";
+        die_value[i][0] = "<img class='die' src='../dice/die" + j + ".png' />";
         die_value[i][1] = j;
         document.getElementById("die" + j).innerHTML = die_value[i][0];
     }
 }
 
 function shuffle_dice() {
+    document.getElementById("math").style.display = "block"
+    let addition = "";
+    let sum = 0;
     for (i = 0, j = 1; i < die_value.length; i++, j++) {
         var rand = Math.floor(Math.random() * 6) + 1;
-        die_value[i][0] = "<img src='../dice/die" + rand + ".png' />"
+        die_value[i][0] = "<img class='die' src='../dice/die" + rand + ".png' />"
         die_value[i][1] = rand;
         document.getElementById("die" + j).innerHTML = die_value[i][0];
-        document.getElementById("numbers").innerHTML = die_value[i][1] + "+" + die_value[j][1];
+        addition += ` ${die_value[i][1]}` + " ";
+        sum =  sum + die_value[i][1];
+        if (i < die_value.length -1) {addition += "+"; } else addition += "= ";
+        
     }
+   
+    document.getElementById("numbers").innerHTML = addition + sum;
+}
+
+function showMath(){
+   
+    var numBlock =  document.getElementById('numbers');
+  if (numBlock.style.display === "none") {
+    numBlock.style.display = "block";
+  } else {
+    numBlock.style.display = "none";
+  }
+
 }
 
 function die_gif() {
     for (i = 0, j = 1; i < die_value.length; j++, i++) {
-        die_value[i][0] = "<img src='../dice/die" + (j) + ".gif' />";
+        die_value[i][0] = "<img class='die' src='../dice/die" + (j) + ".gif' />";
         document.getElementById("die" + j).innerHTML = die_value[i][0];
     }
     setTimeout(shuffle_dice, 600)
