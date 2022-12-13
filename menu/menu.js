9268c246d1d24de6d3801328e5b9b2a053d5c041
chris <chris@chrisedwards.tech>
2022-12-12 21:59:59 -0500

    inporve rollDice and added dynamic menu items - js

--- menu/menu.js
+++ menu/menu.js
@@ -1,4 +1,39 @@
-var navString = '<nav class="navbar is-dark" role="navigation" aria-label="main navigation"> <div class="navbar-brand"> <a class="navbar-item" href="/index.html"> <img src="/cetechwhite.png" width="50" height="50"> </a> <a role="button" class="navbar-burger is-dark" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample"> <span aria-hidden="true"></span> <span aria-hidden="true"></span> <span aria-hidden="true"></span> </a> </div> <div id="navbarBasicExample" class="navbar-menu is-dark"> <div class="navbar-start is-dark"> <a class="navbar-item is-dark" href="/index.html"> Home </a> <a class="navbar-item" href="/resume/" > Resume </a> <div class="navbar-item has-dropdown is-hoverable"> <a class="navbar-link" > Games </a> <div class="navbar-dropdown"> <a class="navbar-item" href="/yatzee/"> Yatzee </a> <a class="navbar-item" href="/tictac/"> Tic Tac Toe </a> <a class="navbar-item" href="/bingo/"> Bingo </a> <a class="navbar-item" href="/memory/"> Memory </a> <a class="navbar-item" href="https://mtg.chrisedwards.tech"> MTG Life Counter </a> </div> </div> <div class="navbar-item has-dropdown is-hoverable is-dark"> <a class="navbar-link" > Ultilities </a> <div class="navbar-dropdown"> <a class="navbar-item" href="/speak"> Speak </a> <a class="navbar-item" href="/binary/"> Binary to ASCII </a> <a class="navbar-item" href="/youtube_convert/"> YouTube Converter </a> <a class="navbar-item" href="/nato/"> NATO Alpha </a> </div> </div> </div> </div> </nav>'
+// var navString = '<nav class="navbar is-dark" role="navigation" aria-label="main navigation"> <div class="navbar-brand"> <a class="navbar-item" href="/index.html"> <img src="/cetechwhite.png" width="50" height="50"> </a> <a role="button" class="navbar-burger is-dark" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample"> <span aria-hidden="true"></span> <span aria-hidden="true"></span> <span aria-hidden="true"></span> </a> </div> <div id="navbarBasicExample" class="navbar-menu is-dark"> <div class="navbar-start is-dark"> <a class="navbar-item is-dark" href="/index.html"> Home </a> <a class="navbar-item" href="/resume/" > Resume </a> <div class="navbar-item has-dropdown is-hoverable"> <a class="navbar-link" > Games </a> <div class="navbar-dropdown"> <a class="navbar-item" href="/yatzee/"> Yatzee </a> <a class="navbar-item" href="/tictac/"> Tic Tac Toe </a> <a class="navbar-item" href="/bingo/"> Bingo </a> <a class="navbar-item" href="/memory/"> Memory </a> <a class="navbar-item" href="https://mtg.chrisedwards.tech"> MTG Life Counter </a> </div> </div> <div class="navbar-item has-dropdown is-hoverable is-dark"> <a class="navbar-link" > Ultilities </a> <div class="navbar-dropdown"> <a class="navbar-item" href="/speak"> Speak </a> <a class="navbar-item" href="/binary/"> Binary to ASCII </a> <a class="navbar-item" href="/youtube_convert/"> YouTube Converter </a> <a class="navbar-item" href="/nato/"> NATO Alpha </a> </div> </div> </div> </div> </nav>'
+let navString = '';
+let baseLinks = [
+                 '<a class="navbar-item is-dark" href="/index.html"> Home </a>',
+                 '<a class="navbar-item" href="/resume/"> Resume </a>'
+];
+let gameLinks = [
+                 '<a class="navbar-item" href="/yatzee/"> Yatzee </a>',
+                 '<a class="navbar-item" href="/tictac/"> Tic Tac Toe </a>',
+                 '<a class="navbar-item" href="/bingo/"> Bingo </a>',
+                 '<a class="navbar-item" href="/memory/"> Memory </a>',
+                 '<a class="navbar-item" href="https://mtg.chrisedwards.tech"> MTG Life Counter </a>'
+];
+let utilLinks = [
+                  '<a class="navbar-item" href="/speak"> Speak </a>',
+                  '<a class="navbar-item" href="/binary/"> Binary to ASCII </a>',
+                  '<a class="navbar-item" href="/youtube_convert/"> YouTube Converter </a>',
+                  '<a class="navbar-item" href="/nato/"> NATO Alpha </a> ',
+                  '<a class="navbar-item" href="/rollDice/"> Roll Dice </a> '
+
+];
+
+navString = '<nav class="navbar is-dark" role="navigation" aria-label="main navigation"> <div class="navbar-brand"> <a class="navbar-item" href="/index.html"> <img src="/cetechwhite.png" width="50" height="50"> </a> <a role="button" class="navbar-burger is-dark" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample"> <span aria-hidden="true"></span> <span aria-hidden="true"></span> <span aria-hidden="true"></span> </a> </div> <div id="navbarBasicExample" class="navbar-menu is-dark"> <div class="navbar-start is-dark"> ';
+for (i = 0; i < baseLinks.length; i++){
+  navString += baseLinks[i];
+}
+navString += '<div class="navbar-item has-dropdown is-hoverable"> <a class="navbar-link"> Games </a> <div class="navbar-dropdown"> ';
+for (i = 0; i < gameLinks.length; i++){
+  navString += gameLinks[i];
+}
+navString += ' </div> </div> <div class="navbar-item has-dropdown is-hoverable is-dark"> <a class="navbar-link"> Ultilities </a> <div class="navbar-dropdown"> ';
+for (i = 0; i < utilLinks.length; i++){
+  navString += utilLinks[i];
+}
+navString += ' </div> </div> </div> </div> </nav>'
+
 document.getElementById('navContainer').innerHTML = navString;
 
 document.addEventListener('DOMContentLoaded', () => {
--- rollDice/dice.js
+++ rollDice/dice.js
@@ -1,6 +1,6 @@
-let numberOfDice = 2;
+let numberOfDice = 6;
 let die_value = [];
-
+let sum = 0;
 function newDiceNumber() {
     die_value = [];
     for (i = 1; i < 7; i++) document.getElementById("die" + i).innerHTML = "";
@@ -17,30 +17,39 @@ function newDiceNumber() {
 function shuffle_dice() {
     document.getElementById("math").style.display = "block"
     let addition = "";
-    let sum = 0;
+    sum = 0;
     for (i = 0, j = 1; i < die_value.length; i++, j++) {
         var rand = Math.floor(Math.random() * 6) + 1;
         die_value[i][0] = "<img class='die' src='../dice/die" + rand + ".png' />"
         die_value[i][1] = rand;
         document.getElementById("die" + j).innerHTML = die_value[i][0];
         addition += ` ${die_value[i][1]}` + " ";
-        sum =  sum + die_value[i][1];
+        sum += + die_value[i][1];
         if (i < die_value.length -1) {addition += "+"; } else addition += "= ";
         
     }
    
     document.getElementById("numbers").innerHTML = addition + sum;
+    return sum;
 }
 
 function showMath(){
-   
     var numBlock =  document.getElementById('numbers');
+    console.log(numBlock.style.display)
   if (numBlock.style.display === "none") {
     numBlock.style.display = "block";
   } else {
     numBlock.style.display = "none";
   }
 
+}
+
+function inputCorrect(){
+    answer = document.getElementById('answer').value;
+    if (answer == sum) {alert('Nice job!'); shuffle_dice();}
+    else if (answer > sum) alert('Sorry, try again, too high...');
+    else if (answer < sum) alert('Sorry, try again, too low...');
+    document.getElementById('answer').value = '';
 }
 
 function die_gif() {
